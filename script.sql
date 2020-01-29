ead90565c91795357c2035687fa8e4e062d378d8
alonso9111 <60263487+alonso9111@users.noreply.github.com>
2020-01-29 09:15:21 -0500

    update DB

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-01-2020 a las 12:40:44
+-- Tiempo de generación: 29-01-2020 a las 15:14:49
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
